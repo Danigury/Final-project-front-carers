aa75fe129ac3588594806afabc8f5156e8723aa9
Dani Sanchez <danysq87@gmail.com>
2021-12-04 16:39:32 +0100

    Hamburguer menu working

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,13 +1,37 @@
+import { Link } from "react-router-dom";
 import "./Header.scss";
 
 const Header = () => {
   return (
     <div className="header-container">
       <header className="header">
+        <div className="menuToggle">
+          <input className="hamburguer" type="checkbox" />
+          <span></span>
+          <span></span>
+          <span></span>
+          <ul className="menu">
+            <li>
+              <Link className="menu__item" to="/location">
+                localizaciones
+              </Link>
+            </li>
+            <li>
+              <Link className="menu__item" to="registro">
+                Registro
+              </Link>
+            </li>
+
+            <li>
+              <Link className="menu__item" to="registro">
+                Registro
+              </Link>
+            </li>
+          </ul>
+        </div>
         <div>
           <h1 className="logo">carers.</h1>
         </div>
-        <div></div>
       </header>
     </div>
   );
