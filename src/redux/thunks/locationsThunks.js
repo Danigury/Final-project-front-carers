4b274f45527004a66382d0d419e58076892bd56a
Dani Sanchez <danysq87@gmail.com>
2021-12-06 11:48:50 +0100

    Change url to connect to db

--- src/redux/thunks/locationsThunks.js
+++ src/redux/thunks/locationsThunks.js
@@ -6,8 +6,7 @@ import {
   updateLocationAction,
 } from "../actions/actionCreators";
 
-// const urlApi = process.env.REACT_APP_URL_API;
-const urlApi = process.env.REACT_APP_URL_LOCAL;
+const urlApi = process.env.REACT_APP_URL_API;
 
 export const loadLocationsThunk = () => {
   return async (dispatch) => {
