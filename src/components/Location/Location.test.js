6a600d10461cdb09674db74ce5f8c3094d9786d8
Dani Sanchez <danysq87@gmail.com>
2021-12-07 15:41:03 +0100

    Location test done

--- src/components/Location/Location.test.js
+++ src/components/Location/Location.test.js
@@ -1,47 +1,42 @@
 import { LocationsToMock } from "../../mocks/mockDetails";
 import { render, screen } from "@testing-library/react";
 import { BrowserRouter as Router } from "react-router-dom";
-import LocationDetail from "./LocationDetail";
+import Location from "./Location";
 import { Provider } from "react-redux";
+import userEvent from "@testing-library/user-event";
 import configureStore from "../../redux/store";
 import Button from "../Button/Button";
-import userEvent from "@testing-library/user-event";
-describe("Given a LocationDetail component", () => {
-  describe("When it receive a location", () => {
+
+describe("Given a Location component", () => {
+  describe("When it receives Location", () => {
     test("Then it should render the location", () => {
       const location = LocationsToMock[0];
       const store = configureStore();
+
       render(
         <Provider store={store}>
           <Router>
-            <LocationDetail location={location} />
+            <Location location={location} />
           </Router>
         </Provider>
       );
       const name = screen.getByRole("heading", { name: location.name });
-      const postcode = screen.getByText(location.address.postcode);
+      const type = screen.getByText(location.type);
       const capacity = screen.getByText(
         `Capacidad para ${location.capacity} personas`
       );
-      const buttonModificar = screen.getByRole("button", { name: "modificar" });
-      const buttonEliminar = screen.getByRole("button", { name: "eliminar" });
-      const buttonLocalizaciones = screen.getByRole("button", {
-        name: "+ localizaciones",
-      });
 
       expect(name).toBeInTheDocument();
+      expect(type).toBeInTheDocument();
       expect(capacity).toBeInTheDocument();
-      expect(postcode).toBeInTheDocument();
-      expect(buttonModificar).toBeInTheDocument();
-      expect(buttonEliminar).toBeInTheDocument();
-      expect(buttonLocalizaciones).toBeInTheDocument();
     });
   });
-  describe("When it receives a buttons with onClick event", () => {
+
+  describe("When it receives a button with onClick event", () => {
     test("Then it should invoke that function", () => {
-      const text = "modificar";
+      const text = "detalles";
       const actionOnClick = jest.fn();
-      const className = "button button-modificar";
+      const className = "button-detalles";
 
       render(
         <Button
@@ -50,8 +45,7 @@ describe("Given a LocationDetail component", () => {
           className={className}
         />
       );
-
-      const button = screen.getByRole("button", { name: "modificar" });
+      const button = screen.getByRole("button", { name: "detalles" });
       userEvent.click(button);
       expect(actionOnClick).toBeCalled();
     });
